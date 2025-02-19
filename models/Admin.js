f7d8888bdafeeb064b8fee34e12761232a4ae75b
Rahul Singh <rahuljsrasr1112@gmail.com>
2025-02-19 18:20:14 +0530

    last commit

--- models/Admin.js
+++ models/Admin.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const adminSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  token: { type: String, required: true },
+  // token: { type: String, required: true },
 });
 
 // Hash password before saving
