61f8fda27664f10436ab4d8959a3aaf656fbb4a1
Rahul Singh <rahuljsrasr1112@gmail.com>
2025-03-17 18:12:09 +0530

    pass word updaate

--- models/Blog.js
+++ models/Blog.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String, required: true},
   content: { type: String, required: true },
   author: { type: String, required: true },
   categories: [{ type: String, required: true }],
